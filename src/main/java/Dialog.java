a2098abf66450454eb21f42a8d876e537c51119f
Vadym <vadym.snigur.code@gmail.com>
2022-02-01 15:49:02 +0200

    first commit

---
+++ src/main/java/Dialog.java
@@ -0,0 +1,14 @@
+public abstract class Dialog {
+    public void renderWindow() {
+        // ... остальной код диалога ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Подклассы будут переопределять этот метод, чтобы создавать конкретные
+     * объекты продуктов, разные для каждой фабрики.
+     */
+    public abstract Button createButton();
+}
---
+++ src/main/java/WindowsDialog.java
@@ -0,0 +1,6 @@
+public class WindowsDialog extends Dialog {
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
